0befe3ee19bda1eeecbc702a4290978063aa913e
houlzh <houlzh@yonyou.com>
2017-12-13 15:28:28 +0800

    Android download

--- android/app/src/main/java/com/appcenter/MainApplication.java
+++ android/app/src/main/java/com/appcenter/MainApplication.java
@@ -3,6 +3,7 @@ package com.appcenter;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.heyao216.react_native_installapk.InstallApkPackager;
 import com.rnfs.RNFSPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new InstallApkPackager(),
             new RNFSPackage()
       );
     }
